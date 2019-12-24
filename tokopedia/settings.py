24c29a41f7e2f33d3661d586e8d5446e548b4611
ArRosid <ahmadrosid30121997@gmail.com>
2019-12-24 08:46:10 +0700

    Create promo spiders

--- tokopedia/settings.py
+++ tokopedia/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'tokopedia.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tokopedia (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- tokopedia/spiders/promo.py
+++ tokopedia/spiders/promo.py
@@ -6,16 +6,17 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class PromoSpider(CrawlSpider):
     name = 'promo'
-    allowed_domains = ['tokopedia.com/promo']
-    start_urls = ['http://tokopedia.com/promo/']
+    allowed_domains = ['tokopedia.com']
+    start_urls = ['https://tokopedia.com/promo']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//div[@class='promotion-cta']/a"), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
+        item['description'] = response.xpath("normalize-space(//h1[@class='post-content__title']/text())").get()
+        item['periode'] = response.xpath("normalize-space(//div[@class='postbox-content-detail postbox-content--period']/p[@class='postbox-content__p'][1]/text())").get()
+        item['minimum_transaction'] = response.xpath("normalize-space(//div[@class='postbox-content-detail postbox-content--min-transaction']/p[@class='postbox-content__p'][1]/text())").get()
+        item['promo_code'] = response.xpath("//input[@class='postbox-content-voucher__input'][1]/@value").get()
         return item
